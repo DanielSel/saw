0a5d3f1bf81159ac3a39438cc624e280db80358a
Daniel Sel <daniel@sel.wtf>
2019-03-24 16:57:22 +0100

    implement pop svc trigger every 10s

--- clients/android/app/src/main/java/net/dsos/sawclient/DebugActivity.kt
+++ clients/android/app/src/main/java/net/dsos/sawclient/DebugActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.content.IntentFilter
 import android.view.View
 import kotlinx.android.synthetic.main.activity_debug.*
+import android.content.Intent
+
+
 
 
 class DebugActivity() : AppCompatActivity() {
 
-    private val popSvc: PopService = PopService();
     private val sawGrpcClient = SawGrpcClient();
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +25,17 @@ class DebugActivity() : AppCompatActivity() {
 
     public fun sendPop(view: View) {
         sawGrpcClient.sendPop()
+    }
+
+    public fun startPopSvc(view: View) {
+        val startIntent = Intent(this, PopService::class.java)
+        startIntent.action = PopService.ACTION_START_FOREGROUND_SERVICE
+        startService(startIntent)
+    }
+
+    public fun stopPopSvc(view: View) {
+        val stopIntent = Intent(this, PopService::class.java)
+        stopIntent.action = PopService.ACTION_STOP_FOREGROUND_SERVICE
+        startService(stopIntent)
     }
 }
